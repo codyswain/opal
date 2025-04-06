8f6e2d0086e069e796ec943201dc953ca8b3512f
codyswain <swain.cody@gmail.com>
2025-04-06 00:01:37 -0700

    remove old notes

--- src/renderer/features/navbar/config/navbarItems.ts
+++ src/renderer/features/navbar/config/navbarItems.ts
@@ -5,7 +5,6 @@ const navbarItems: NavbarItemProps[] = [
   // TODO: add this in
   // { to: "/", icon: Home, text: "Home" },
   { to: "/explorer", icon: Book, text: "Explorer"},
-  { to: "/feed", icon: Clock, text: "Feed"}
 ];
 
 export default navbarItems;
